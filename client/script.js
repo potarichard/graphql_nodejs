1d530fb09b9c6bf3523640aa9c5f020590245c11
myname <richard.i.pota@gmail.com>
2020-08-31 12:36:05 +0200

    graphql tested

--- client/script.js
+++ client/script.js
@@ -1,50 +1,52 @@
 const continentSelect = document.getElementById('continent-select')
 const countryList = document.getElementById('countries-list')
 
-queryFetch(
-    `query {
-        continents {
-            name
-            code
-        }
-    }`
-    ).then(data => {
-  data.data.continents.forEach(continent => {
-    const option = document.createElement('option')
-    option.value = continent.code
-    option.innerText = continent.name
-    continentSelect.append(option)
-  })
-})
+const query_continents = 
+`query {
+  continents {
+      name
+      code
+  }
+}`
 
-continentSelect.addEventListener('change', async e => {
+const query_with_variable = 
+`query getCountries($code: ID!) {
+  continent(code: $code) {
+    countries {
+      name
+    }
+  }
+}`
+
+queryFetch(query_continents).then(data => { data.data.continents.forEach(continent => 
+      {
+        const option = document.createElement('option')
+        option.value = continent.code
+        option.innerText = continent.name
+        continentSelect.append(option)
+      })
+    })
+
+continentSelect.addEventListener('change', async e => 
+{
   const continentCode = e.target.value
   const countries = await getContinentCountries(continentCode)
   countryList.innerHTML = ''
-  countries.forEach(country => {
-    const element = document.createElement('div')
-    element.innerText = country.name
-    countryList.append(element)
-  })
+
+  countries.forEach(country => 
+    {
+      const element = document.createElement('div')
+      element.innerText = country.name
+      countryList.append(element)
+    })
 })
 
-function getContinentCountries(continentCode) {
-    console.log(continentCode)
-  return queryFetch(
-    `query getCountries($code: ID!) {
-      continent(code: $code) {
-        countries {
-          name
-        }
-      }
-    }`
-    , { code: continentCode }).then(data => 
-        { 
-            console.log(data)
-            return data.data.continent.countries
-        })
+function getContinentCountries(continentCode) 
+{   
+  return queryFetch(query_with_variable, { code: continentCode }).then(data => data.data.continent.countries);
 }
 
+// ez egy promiset ad vissza jsonbe
 function queryFetch(query, variables) {
   return fetch('https://countries.trevorblades.com/', {
     method: 'POST',
